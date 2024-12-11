fe2984269f42ad8a6ebc2133b7e9d97627008839
sebastianosalzillo <salzillosebastiano@gmail.com>
2024-12-11 18:14:03 +0100

    feat: struttura completata senza csss

--- react-task-list/src/App.jsx
+++ react-task-list/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import tasks from './tasks';
+import './app.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+ 
+
+
+
+  const taskListBacklog = () => {
+
+    const filteredTasks = tasks.filter(task => task.state === "backlog" || task.state === "in_progress");
+
+
+    return filteredTasks.map(task => <li key={task.id}>
+
+        <h3>{task.title} </h3>
+        <section>
+        <p>Priorita : {task.priority}</p>
+        <p>Tempo stimato : {task.estimatedTime}</p>
+        </section>
+        
+      </li>
+
+      );
+  };
+
+
+
+  const taskListCompleted = () => {
+    const filteredTasks = tasks.filter(task => task.state === "completed");
+
+    return filteredTasks.map(task => <li key={task.id}>
+        <h3>{task.title}</h3>
+        <p>Priorita : {task.priority}</p>
+        <p>Tempo stimato : {task.estimatedTime}</p>
+      </li>)
+  };
+
+
+      return (
+      <>
+        <div className='title'>
+          <h1>Gestore Task</h1>
+        </div>
+        <div>
+          <h2>Task in progresso</h2>
+          <ul>{taskListBacklog()}</ul>
+        </div>
+        <hr />
+        <div>
+          <h2>Task completate</h2>
+          <ul>{taskListCompleted()}</ul>
+        </div>
+      </>
+      );
 }
 
-export default App
+      export default App;
